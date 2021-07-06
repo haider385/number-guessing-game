40e48ecb33df4eabf648b02c7e722c76f12db25f
Haider <haider.ali385@gmail.com>
2021-07-06 17:34:07 +0100

    edited to code to meet pep8 guidelines

--- number-guessing-game.py
+++ number-guessing-game.py
@@ -1,13 +1,12 @@
 from random import randint
 
 
-
-def initialize(highscore = None):
+def initialize(highscore=None):
     num_guesses = 0
     current_guess = 0
-    number = randint(1,10)
-    print("""\n\nHello!\nWelcome to the Number guessing game.\nI'm thinking of a number between 1 and 10.
-Can you guess it?
+    number = randint(1, 10)
+    print("""\n\nHello!\nWelcome to the Number guessing game.
+I'm thinking of a number between 1 and 10. Can you guess it?
     """)
     if not highscore:
         highscore = 0
@@ -26,25 +25,28 @@ def game(current_guess, number, num_guesses, highscore):
             print("Please enter an integer!\n")
             continue
         else:
-            if current_guess not in range(1,11):
+            if current_guess not in range(1, 11):
                 print("Please guess a number between 1 and 10!\n")
                 continue
             num_guesses += 1
 
         if current_guess > number:
-            print("The number is lower than {}, try again!\n".format(current_guess))
+            print("The number is lower than {}, try again!\n".format(
+                current_guess))
         elif current_guess < number:
-            print("The number is higher than {}, try again!\n".format(current_guess))
+            print("The number is higher than {}, try again!\n".format(
+                current_guess))
         else:
-            print("""\nCongratulations! The number was {}. It took you {} attempts to get it right.""".format(
-        number, num_guesses))
+            print("""\nCongratulations! The number was {}.
+It took you {} attempts to get it right.""".format(
+                 number, num_guesses))
             if highscore:
                 if num_guesses < highscore:
                     highscore = num_guesses
             else:
                 highscore = num_guesses
     return highscore
-            
+
 
 def main():
     playing = True
@@ -53,7 +55,8 @@ def main():
         current_guess, number, num_guesses, highscore = initialize(highscore)
         highscore = game(current_guess, number, num_guesses, highscore)
         try:
-            decision = input("Enter 'Y' if you would like to play again.\n").lower()
+            decision = input(
+                "Enter 'Y' if you would like to play again.\n").lower()
         except:
             playing = False
 
@@ -62,6 +65,3 @@ def main():
             break
 
 main()
-        
-
-
